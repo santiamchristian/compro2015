1834d67818fd9e0d44fa0ceed08de8516c621b70
J-Swagg <jeremichael.paul@gmail.com>
2015-02-06 13:49:21 -0800

    Added imput controller

---
+++ ComPro2015/Assets/Scripts/InputController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+    public MovementController[] players;
+
+    void Start()
+    {
+        players = gameObject.GetComponentsInChildren<MovementController>();
+    }
+
+    void Update()
+    {
+        if (players[0] != null)
+        {
+            players[0].Move(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+            if (Input.GetButton("Jump")) players[0].Jump();
+        }
+    }
+}
