5837acda6c3b257e7652866dbb5a7175d205f3fc
J-Swagy <tindleje@gmail.com>
2015-02-10 14:12:57 -0800

    really added health script

--- ComPro2015/Assets/Scripts/Health.cs
+++ ComPro2015/Assets/Scripts/Health.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour
+{
+    public int hp = 100;
+    public bool isEnemy = true;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Damage(1);
+    }
+
+
 }
+
