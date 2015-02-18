4df11c9add882eae81d17aa83962c0afd43132d8
J-Swagg <jeremichael.paul@gmail.com>
2015-02-18 15:12:47 -0800

    Added Camera Script

---
+++ ComPro2015/Assets/Scripts/LevelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        loadLevel("Terrain");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject debug = GameObject.Find("Debug");
+        if (debug != null)
+            Destroy(debug);
+	}
+
+    void loadLevel(string levelName)
+    {
+        Application.LoadLevelAdditive(levelName);
+
+    }
+}
