7b328162af3f05e30919824085b166a29c347af0
B-Swag <blakepeterson2014@gmail.com>
2015-03-18 14:33:09 -0700

    Fixed player gui

---
+++ ComPro2015/Assets/Scripts/PlayerGui.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGui : MonoBehaviour {
+
+    public Transform playerContainer;
+    public RectTransform[] guiPositions = new RectTransform[5];
+    private int previousPlayerCount =0  ;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        previousPlayerCount = playerContainer.transform.childCount;
+	}
+
+    void playerGui()
+    {
+        if (transform.childCount > previousPlayerCount)
+        {
+             
+        }
+    }
+}
