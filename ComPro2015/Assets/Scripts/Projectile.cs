1d4b5f36538093e0b9e35711375a31e31d9dae4b
B-Swag <blakepeterson2014@gmail.com>
2015-04-14 13:50:34 -0700

    fixed wind gust directional motion (stupid gravity defect)

--- ComPro2015/Assets/Scripts/Projectile.cs
+++ ComPro2015/Assets/Scripts/Projectile.cs
@@ -49,7 +49,7 @@ public class Projectile : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
             if (hit.transform.tag == "Terrain")
-                transform.position =Vector3.Slerp(transform.position, new Vector3 (transform.position.x, transform.position.y - (hit.distance - distanceFromGround), transform.position.z), dropSpeed);
+                transform.position = new Vector3 (transform.position.x, transform.position.y - (hit.distance - distanceFromGround), transform.position.z);
 
     }
 }
