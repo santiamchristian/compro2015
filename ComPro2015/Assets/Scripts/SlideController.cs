988f44e4c072c0e9a9ff6639b99b7d87444eb9e2
J-Swagg <jeremichael.paul@gmail.com>
2015-04-16 14:27:36 -0700

    Added Sliding Script

---
+++ ComPro2015/Assets/Scripts/SlideController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideController : MonoBehaviour {
+    public float slideSpeed;
+    public bool slidingDown { get; set; }
+
+    void Update()
+    {
+        if (slidingDown)
+        {
+
+            ((RectTransform)transform).anchoredPosition = Vector3.Slerp(((RectTransform)transform).anchoredPosition, Vector3.zero, slideSpeed);
+            if (((RectTransform)transform).anchoredPosition.y <= 0)
+            {
+                slidingDown = false;
+            }
+        }
+    }
+}
