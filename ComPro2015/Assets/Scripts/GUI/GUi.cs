68d303cf76f5d8be054b6e56dda09b74cf185088
J-Swagg <jeremichael.paul@gmail.com>
2015-05-06 15:04:43 -0700

    Sweg

--- ComPro2015/Assets/Scripts/GUI/GUi.cs
+++ ComPro2015/Assets/Scripts/GUI/GUi.cs
@@ -5,9 +5,26 @@ using UnityEngine.UI;
 public class GUi : MonoBehaviour {
     public RectTransform lifeBar;
     public RectTransform[] abilities = new RectTransform[4];
+    public float LifeBarWidth;
+
+    public void Start()
+    {
+        LifeBarWidth = lifeBar.anchorMax.x;
+
+    }
 
     public void SetAbilityCooldown(float cooldown, int ability)
     {
         abilities[ability].GetComponent<Image>().color = new Color(1, 1, 1, Mathf.Clamp(cooldown, 0, 1));
+    }
+
+    public void SetHealthBar(float health, float maxHealth)
+    {
+        lifeBar.anchorMax = new Vector2((health / maxHealth) * LifeBarWidth, lifeBar.anchorMax.y);
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
--- ComPro2015/Assets/Scripts/Player/Health.cs
+++ ComPro2015/Assets/Scripts/Player/Health.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class Health : MonoBehaviour
 {
     public int hp = 100;
+    int maxHP;
     public bool isEnemy = true;
     public int playerIndex;
+    public GUi gui;
 
     // Use this for initialization
     public void Damage(int damageCount)
@@ -14,19 +16,22 @@ public class Health : MonoBehaviour
 
         if (hp <= 0)
         {
+            gui.Destroy();
             Destroy(gameObject);
         }
     }
 
     void Start()
     {
+        maxHP = hp;
         playerIndex = transform.GetComponent<Player>().playerIndex;
+        gui = transform.GetComponent<Player>().playerGUI;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        gui.SetHealthBar(hp, maxHP);
     }
 
 
