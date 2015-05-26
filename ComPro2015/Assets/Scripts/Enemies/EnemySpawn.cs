94de1e7021c8d4449954b4c0730b28c403c7fd7e
A-Swag <bowhunter1398@gmail.com>
2015-05-26 14:19:35 -0700

    Enemies now spawn in waves

--- ComPro2015/Assets/Scripts/Enemies/EnemySpawn.cs
+++ ComPro2015/Assets/Scripts/Enemies/EnemySpawn.cs
@@ -7,30 +7,9 @@ public class EnemySpawn : MonoBehaviour {
     public GameObject enemyPrefab; 
     public int wave;
     public float spawnTimer;
-    private float elapsedTime;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (enemyContainer == null)
-        {
-            enemyContainer = GameObject.Find("Enemies");
-        }
-        else {
-            if (elapsedTime >= spawnTimer)
-            {
-                elapsedTime = 0;
-                SpawnEnemies();
-            }
-        }
-        elapsedTime += Time.deltaTime; 
-	}
 
-    void SpawnEnemies()
+    public void SpawnEnemies()
     {
         GameObject newEnemy = Instantiate(enemyPrefab, transform.position, transform.rotation) as GameObject;
         newEnemy.transform.parent = enemyContainer.transform;
